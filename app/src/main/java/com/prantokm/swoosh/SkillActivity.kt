882f4b63e3aebc437388576132e960601c7160da
PrantoMondal <prantomondal517@gmail.com>
2021-11-16 21:00:43 +0600

    modified league activity

--- app/src/main/java/com/prantokm/swoosh/SkillActivity.kt
+++ app/src/main/java/com/prantokm/swoosh/SkillActivity.kt
@@ -3,7 +3,7 @@ package com.prantokm.swoosh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SkillActivity : AppCompatActivity() {
+class SkillActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
